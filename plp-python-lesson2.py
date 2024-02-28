8e1bd6fb4f3107caf53195ec94e641becbada618
rayhuba <huba855@gmail.com>
2024-02-28 16:22:51 +0300

    this is my second class in plp

---
+++ plp-python-lesson2.py
@@ -0,0 +1,36 @@
+a=100
+b=3.14
+print(a*b)
+c=5+7j
+print(a+b-c)
+d=False
+print(d)
+print(a/b)
+str = 'Welcome to PLP Academy'
+print(str)
+print(str[0])
+print(str[-1])
+print(str[4:10])
+plp="Python Class"
+print(str +  plp)
+#this is a commment in python
+shopping_list =['bread','milk',20,40.5,5+8j]
+print(shopping_list)
+#Building_List =[450,'bread,'wheelbarrows',180,'shovels']
+#print(Building_List)
+print(shopping_list[0])
+print(shopping_list[4:5])
+plp_tuple='abcd',786.2,33,'john,70,2'
+print(plp_tuple)
+tiny_tuple=('John',5+7j)
+print(tiny_tuple)
+dict ={}
+dict['one']="This is one"
+dict[2]="This is two"
+tinydict ={'name':'john','code':6743,'dept':1007}
+print(dict['one'])
+print(dict[2])
+print(tinydict.keys())
+print(tinydict.values())
+
+
